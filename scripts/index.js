ebffc8bada3224f20aca839989c0bf1353ed9bdf
Amenonuhoko <mauriza.arianne@gmail.com>
2021-01-09 17:20:20 +1100

    initial

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+const navFunction = () => {
+	var x = document.getElementById("navigation");
+	if (x.style.display === "flex") {
+		x.style.display = "none";
+	} else {
+		x.style.display = "flex";
+	}
+	return false;
+};
